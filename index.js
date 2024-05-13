c371ca23c384910f3672980e637e4fb558b7a4ad
coolkiddzzz <mwaiyee96@gmail.com>
2024-05-13 21:56:01 +0800

    Edit index.js file

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
-module.exports.firsthandler = async (event) => {
-    console.log('Start')
-    console.log(event)
-    console.log('End')
-    return {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: "This is get function!",
-        },null,2),
-    };
-  };
+// module.exports.firsthandler = async (event) => {
+//     console.log('Start')
+//     console.log(event)
+//     console.log('End')
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(
+//         {
+//           message: "This is get function!",
+//         },null,2),
+//     };
+//   };
   
   
-  module.exports.handler_hello = async (event) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: "Hello there!",
-          input: event,
-        },
-        null,
-        2
-      ),
-    };
-  };
-  
+//   module.exports.handler_hello = async (event) => {
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(
+//         {
+//           message: "Hello there!",
+//           input: event,
+//         },
+//         null,
+//         2
+//       ),
+//     };
+//   };
+
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+// Define a route handler for the root path
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+// Start the server
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = server; // Export the server instance
--- index.test.js
+++ index.test.js
@@ -1,15 +1,26 @@
-const { firsthandler } = require('./index')
+// const { firsthandler } = require('./index')
 
-describe('firsthandler', () => {
-  it('should return status code 200 and the correct message', async () => {
-    const event = {};
-    const expectedResponse = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "This is get function!",
-      }, null, 2),
-    };
-    const result = await firsthandler(event);
-    expect(result).toEqual(expectedResponse);
+// describe('firsthandler', () => {
+//   it('should return status code 200 and the correct message', async () => {
+//     const event = {};
+//     const expectedResponse = {
+//       statusCode: 200,
+//       body: JSON.stringify({
+//         message: "This is get function!",
+//       }, null, 2),
+//     };
+//     const result = await firsthandler(event);
+//     expect(result).toEqual(expectedResponse);
+//   });
+// });
+
+const request = require('supertest');
+const app = require('./index');
+
+describe('GET /', () => {
+  it('responds with "Hello, world!"', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello, world!');
   });
 });
